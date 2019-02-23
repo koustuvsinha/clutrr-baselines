4555408952868f5ed1bda54830e5115d052fc996
Koustuv Sinha <koustuvsinha@hotmail.com>
2019-02-23 13:59:01 -0500

    stdout reader update

--- codes/stdout_reader.py
+++ codes/stdout_reader.py
@@ -8,6 +8,7 @@ import glob
 if __name__ == '__main__':
     # list all the slurm outputs
     results = glob.glob('*.log')
+    print("Found {} logs".format(len(results)))
     rows = []
     for res in results:
         r_file = open(res).readlines()
@@ -19,3 +20,5 @@ if __name__ == '__main__':
                 rows.append({'file': file_name, 'test_acc':m_test_acc})
     df = pd.DataFrame(rows)
     df.to_csv('hyp_results.csv')
+    print("Best hyperparams : ")
+    print(df.groupby(['file'], sort=False)['test_acc'].max())
